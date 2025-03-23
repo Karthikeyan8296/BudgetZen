52785f44e3137ecbbe9e762a6306bc42d3138765
Karthikeyan <165583967+Karthikeyan8296@users.noreply.github.com>
2025-03-23 10:28:50 +0530

    initial setup

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { View, Text, StatusBar } from "react-native";
+import React from "react";
+import "@/global.css";
+
+const index = () => {
+  return (
+    <View>
+      <StatusBar barStyle="dark-content" />
+      <Text>index</Text>
+    </View>
+  );
+};
+
+export default index;
