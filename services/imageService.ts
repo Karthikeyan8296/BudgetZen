a596a9088074d51319add37478b181be6f3d7c8a
Karthikeyan <165583967+Karthikeyan8296@users.noreply.github.com>
2025-04-17 20:46:48 +0530

    cloudinary upload

--- services/imageService.ts
+++ services/imageService.ts
@@ -1,3 +1,49 @@
+import { CLOUDINARY_CLOUD_NAME, CLOUDINARY_UPLOAD_PRESET } from "@/config";
+import { ResponseType } from "@/types";
+import axios from "axios";
+
+const CLOUDINARY_CLOUD_URL = `https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/image/upload`;
+
+//upload file to cloudinary
+export const uploadFileCloudinary = async (
+  file: { uri?: string } | string,
+  folderName: string
+): Promise<ResponseType> => {
+  try {
+    if (typeof file == "string") {
+      return { success: true, data: file };
+    }
+
+    if (file && file.uri) {
+      const formData = new FormData();
+      formData.append("file", {
+        uri: file?.uri,
+        type: "image/jpeg",
+        name: file?.uri?.split("/").pop() || "file.jpg",
+      } as any);
+
+      formData.append("upload_preset", CLOUDINARY_UPLOAD_PRESET);
+      formData.append("folder", folderName);
+
+      //api request to upload this file
+      const response = await axios.post(CLOUDINARY_CLOUD_URL, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+
+      // console.log("upload image res ", response.data);
+
+      return { success: true, data: response?.data?.secure_url };
+    }
+    return { success: true };
+  } catch (error: any) {
+    console.log("Got error in uploading file : ", error);
+    return { success: false, msg: error.message || "Could not upload file" };
+  }
+};
+
+//get the images service
 export const getProfileImage = (file: any) => {
   if (file && typeof file == "string") return file;
   if (file && typeof file == "object") return file.uri;
--- services/userService.ts
+++ services/userService.ts
@@ -1,12 +1,32 @@
 import { firestore } from "@/config/firebase";
 import { ResponseType, UserDataType } from "@/types";
 import { doc, updateDoc } from "firebase/firestore";
+import { uploadFileCloudinary } from "./imageService";
 
 export const updateUser = async (
   uid: string,
   updatedDate: UserDataType
 ): Promise<ResponseType> => {
   try {
+    //image update
+    if (updatedDate.image && updatedDate?.image?.uri) {
+      const imageUploadRes = await uploadFileCloudinary(
+        updatedDate.image,
+        "users"
+      );
+
+      if (!imageUploadRes.success) {
+        return {
+          success: false,
+          msg: imageUploadRes.msg || "Failed to upload image",
+        };
+      }
+
+      //if image uploaded
+      updatedDate.image = imageUploadRes.data;
+    }
+
+    //username update
     //change the user data in the firestore doc
     const userRef = doc(firestore, "users", uid);
     await updateDoc(userRef, updatedDate);
